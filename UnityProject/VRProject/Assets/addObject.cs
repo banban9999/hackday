960246e73fcce73e1d737e5e8d295850cdcb8f5a
Kaoru Beppu <beppu@f.ait.kyushu-u.ac.jp>
2019-12-15 00:15:30 +0900

    add raser

--- UnityProject/VRProject/Assets/addObject.cs
+++ UnityProject/VRProject/Assets/addObject.cs
@@ -7,18 +7,12 @@ public class addObject : MonoBehaviour
 
     public static int count = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 1; i++) {
             GameObject obj = (GameObject)Resources.Load ("Player");
-            GameObject instance = Instantiate (obj, new Vector3(Random.value * 10, Random.value * 10 + 15, Random.value * 10), Quaternion.identity);
+            GameObject instance = Instantiate (obj, new Vector3(Random.value * 20 + 5, Random.value * 10 + 15, Random.value * 10), Quaternion.identity);
             float rColor = Random.value;
             float gColor = Random.value;
             float bColor = Random.value;
