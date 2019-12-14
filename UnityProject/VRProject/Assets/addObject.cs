38454bdcf6d8229b5300084f43ede7413ac5ff15
Kaoru Beppu <beppu@f.ait.kyushu-u.ac.jp>
2019-12-15 06:11:50 +0900

    change enemy faces

--- UnityProject/VRProject/Assets/addObject.cs
+++ UnityProject/VRProject/Assets/addObject.cs
@@ -12,12 +12,13 @@ public class AddObject : MonoBehaviour
     {
         if (count % 100 == 0) {
             GameObject obj = (GameObject)Resources.Load ("Enemy");
-            GameObject instance = Instantiate (obj, new Vector3(Random.value * 20 + 5, Random.value * 10 + 15, Random.value * 10), Quaternion.identity);
+            GameObject instance = Instantiate (obj, new Vector3(Random.value * 30, Random.value * 20 + 5, Random.value * 20), Quaternion.identity);
             float rColor = Random.value;
             float gColor = Random.value;
             float bColor = Random.value;
-            instance.GetComponent<Renderer>().material.color = new Color(rColor, gColor, bColor, 1.0f);
-            // instance.GetComponent<Transform>().localScale = new Vector3(Random.value * 0.5f + 1.0f, Random.value * 0.5f + 1.0f, Random.value * 0.5f + 1.0f);
+            foreach(Transform child in instance.GetComponent<Transform>()) {
+                child.GetComponent<Renderer>().material.color = new Color(rColor, gColor, bColor, 1.0f);
+            }
         }
         count++;
     }
